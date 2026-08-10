7a5f523e924f7215e28bc6f7faa62874cafa2fe5
Tony Mourrah <Tony.mourrah.1@ens.etsmtl.ca>
2026-08-10 19:08:50 -0400

    Simplification systeme reveil serveur, ajout Settings et affichage utilisateu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider, useAuth } from "./context/AuthContext";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Reallocation from "./pages/Reallocation";
-import Transactions from "./pages/Transactions";
-import ServerWakingOverlay from "./components/ServerWakingOverlay";
-import Settings from "./pages/Settings";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './context/AuthContext';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Reallocation from './pages/Reallocation';
+import Transactions from './pages/Transactions';
+import Settings from './pages/Settings';
+
 function PrivateRoute({ children }) {
   const { isAuthenticated } = useAuth();
   return isAuthenticated ? children : <Navigate to="/login" />;
@@ -21,46 +21,10 @@ function AppRoutes() {
       <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/reallocation"
-        element={
-          <PrivateRoute>
-            <Reallocation />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/transactions"
-        element={
-          <PrivateRoute>
-            <Transactions />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/settings"
-        element={
-          <PrivateRoute>
-            <Settings />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/settings"
-        element={
-          <PrivateRoute>
-            <Settings />
-          </PrivateRoute>
-        }
-      />
+      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      <Route path="/reallocation" element={<PrivateRoute><Reallocation /></PrivateRoute>} />
+      <Route path="/transactions" element={<PrivateRoute><Transactions /></PrivateRoute>} />
+      <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
     </Routes>
   );
 }
@@ -69,7 +33,6 @@ export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <ServerWakingOverlay />
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
